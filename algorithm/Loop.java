24b3e7bafc64ad1efda50c6a1dadaf3467ea3bce
martyn-su <99847230+martyn-su@users.noreply.github.com>
2022-08-11 19:02:37 +0300

    For loop and For each
    For loop and For each
    Iterating through an array of strings

---
+++ algorithm/Loop.java
@@ -0,0 +1,17 @@
+public class Loop {
+    public static void main(String[] args) {
+        System.out.println("Looping through an array of strings...");
+        String[] arrNames = { "Jane", "Jude", "Judy", "Joe", "Jake" };
+        System.out.println("Printing them out using a for-each...");
+
+        for (String item : arrNames) {
+            System.out.println(item);
+        }
+
+        System.out.println("Printing them out using a for-loop...");
+        int arrName = arrNames.length;
+        for (int i = 0; i < arrName; i++) {
+            System.out.println(arrNames[i]);
+        }
+    }
+}
